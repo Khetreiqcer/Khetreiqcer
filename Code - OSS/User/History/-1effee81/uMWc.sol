f65928ac7d821389eeba621c5fa025dde45de177
João Vitor Possenti <jvpossenti666@gmail.com>
2024-11-05 20:51:56 -0300

    Delete Code - OSS directory

--- Code - OSS/User/History/-1effee81/uMWc.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
-
-import "forge-std/Script.sol";
-import "../src/SimpleStorage.sol";
-
-contract DeploySimpleStorage is Script {}
--- Code - OSS/User/History/4dda7c39/R82V.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.8.19;
-
-import {Test} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-
-contract
